813cf5fc0916aed3fb2a2e013b506f9ef490c629
Jay <wnwoghd22@gmail.com>
2023-01-19 21:12:03 +0900

    pagination
    복잡하게 흩어졌던 스크립트 조각들을 모으고, 여러 개의 글을 모두 확인할 수 있도록 페이지를 넘기는 기능도 추가했다. 이제 좀 더 예쁘게 만들자.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,51 @@
 var params = {};
+var current_category = "";
+var page_length = 10;
+var index = 0;
+var all_items;
+var items;
+var page_num; 
+
+init = () => {
+    items = document.getElementsByClassName('post-item' + ' ' + current_category);
+    console.log(items)
+    console.log(items.length)
+    page_num = Math.ceil(items.length / page_length);
+}
+
+setIndexByElement = n => {
+    index = n;
+
+    console.log('postpage' + ' ' + current_category);
+
+    Array.from(items).forEach(element => {
+        element.style.display = 'none';
+    });
+
+    for(let i = n * page_length; i < Math.min((n + 1) * page_length, items.length); ++i) {
+        items[i].style.display = 'block';
+    }
+
+    let postion = document.getElementById('indexer');
+    postion.innerText= String(index+1) + "/" + String(page_num);
+}
+
+SetCategory = c => {
+    current_category = c;
+
+    index = 0;
+    all_items = document.getElementsByClassName('post-item');
+    Array.from(all_items).forEach(element => {
+        element.style.display = 'none';
+    });
+}
+
+previousPage = () => {
+    if(index > 0) setIndexByElement(--index);
+}
+nextPage = () => {
+    if(index < page_num - 1) setIndexByElement(++index);
+}
 
 GetParams = () => {
     let url = location.href;
@@ -18,4 +65,5 @@ GetParams();
 
 if('category' in params)
     SetCategory(params['category']);
+init();
 setIndexByElement(0);
